840c95d0119b1bf8df0d7a81849b0f04345e5e1e
Blaise202 <iblaise2023@gmail.com>
2025-11-13 23:55:17 +0000

    JUnit test runs

--- TestEvent.java
+++ TestEvent.java
@@ -7,20 +7,54 @@ public class TestEvent {
 
     @Test
     public void testConcertEventCreation() {
-        String[] performers = {"Artist A", "Artist B"};
+        String[] performers = {"Blacko", "Kofi"};
         ConcertEvent concert = new ConcertEvent(
             "Music Night",
-            "Auditorium",
+            "The Hive",
             LocalDate.of(2025, 11, 25),
             LocalTime.of(19, 0),
             "2 hours",
             performers
         );
 
-        // Assertions to check data
+        // Assertions to check concert event data
         assertEquals("Music Night", concert.getName());
-        assertEquals("Auditorium", concert.getLocation());
-        assertEquals("Artist A", concert.getPerformers()[0]);
-        assertEquals("Artist B", concert.getPerformers()[1]);
+        assertEquals("The Hive", concert.getLocation());
+        assertEquals("Blacko", concert.getPerformers()[0]);
+        assertEquals("Kofi", concert.getPerformers()[1]);
     }
+
+    @Test
+    public void testSportsEventCreation() {
+        SportsEvent match1 = new SportsEvent(
+            "ASHESI Games Final",
+            "Ashesi Sports Complex",
+            LocalDate.of(2024, 11, 2),
+            LocalTime.of(15, 30),
+            "2 hours",
+            new String[] {"Elite FC", "Kasanoma"},
+            "Ashesi Sports Complex"
+        );
+
+        assertArrayEquals(new String[]{"Elite FC", "Kasanoma"}, match1.getTeams());
+        assertEquals("Ashesi Sports Complex", match1.getDetails());
+        
+
+    }
+
+    @Test
+    public void testPartyEventCreation() {
+        PartyEvent party = new PartyEvent(
+            "End of Semester Beach Party",
+            "Labadi Beach",
+            LocalDate.of(2024, 12, 7),
+            LocalTime.of(12, 0),
+            "All day",
+            "Tropical Paradise"
+        );
+
+        assertEquals("Tropical Paradise", party.getTheme());
+        assertEquals("Labadi Beach", party.getLocation());
+    }
+
 }
